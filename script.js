96bedad881c2e9a46dc6ed61b1819075b696cb0d
Jake Dam <robert.dam2@gmail.com>
2020-11-11 00:11:49 -0500

    finished genGrid and color change functions

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ const container = document.getElementById("grid-container");
 
 // Function to generate grid
 function genGrid(x) {
-    for(i = 0; i < x; i++){
-        for(j = 0; j < x; j++) {
+    for(var i = 0; i < x; i++){
+        var gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        for(var j = 0; j < x; j++) {
             var gridSquare = document.createElement("div"); 
-            gridSquare.className = "grid-square";
-            container.appendChild(gridSquare);
-            console.log("new square");
+            gridSquare.classList.add("grid-square");
+            gridRow.appendChild(gridSquare);
         }
-    }
-    var newRow = document.createElement("br"); 
-    container.appendChild(newRow);
+        container.appendChild(gridRow);
+    } 
 }
-   
+
+
+// Function to add color change listener to specific class 
+function colorChangeEventListener(color) {
+    var nodeList = document.querySelectorAll(".grid-square");
+    for(var i = 0, len = nodeList.length; i < len; i++) {
+        nodeList[i].addEventListener(
+            "mouseover", function() {
+                this.style.backgroundColor = color;
+            });
+    }
+}
 
 genGrid(16);
+
+// Change grid-square color on mouseover
+colorChangeEventListener("black");
